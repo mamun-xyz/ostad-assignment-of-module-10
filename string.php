35d9ea63a00d986fc994b8b0ed1adf0afcbbc678
Abdullah Al Mamun <themamunxyz@gmail.com>
2024-08-22 12:57:46 +0600

    Ostad Assignment of Module-10

---
+++ string.php
@@ -0,0 +1,24 @@
+<?php
+$strings = ["Hello", "World", "PHP", "Programming"];
+function countVowels($str)
+{
+    $vowels = ["a", "e", "i", "o", "u"];
+    $count = 0;
+
+    $str = strtolower($str);
+
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (in_array($str[$i], $vowels)) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+foreach ($strings as $string) {
+    $vowelCount = countVowels($string);
+    $reversedString = strrev($string);
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
+
+?>
